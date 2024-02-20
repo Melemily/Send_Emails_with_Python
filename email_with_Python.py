3f159febdca79b99cc5ba3b0de9aab9346b91166
Melissa Phillips <121769717+Melemily@users.noreply.github.com>
2024-02-20 15:42:49 -0500

    Update email_with_Python.py
    Added scheduled send code and clarified variable names

--- email_with_Python.py
+++ email_with_Python.py
@@ -1,9 +1,11 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+#import schedule // still testing functionality on schedule send
+#import time
 
 email_sender = "your.email@provider.com" # use email you'd like to send the message from.
-email_password = "" #Insert 16-digit App password code found in 2-step verification section of Gmail. Redacted for security purposes. 
+app_password = "[enter app password here]" #Insert 16-digit App password code found in 2-step verification section of Gmail. Redacted for security purposes. 
 email_receiver = "your.email@provider.com" # use email you'd like to send message to.
 
 subject = "insert subject here"
@@ -22,5 +24,12 @@ context = ssl.create_default_context()
 
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context = context) as smtp:
-    smtp.login(email_sender, email_password)
+    smtp.login(email_sender, app_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+# Schedule the job to run daily at a specific time (adjust as needed)
+#schedule.every().day.at("08:00").do(send_email)
+
+#while True:
+    #schedule.run_pending()
+    #time.sleep(1)
